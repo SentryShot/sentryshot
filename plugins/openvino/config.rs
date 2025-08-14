496dc6dcae4cd3820d6549f6ecb796697dd6f651
Henry Ou <henryouly@gmail.com>
2025-08-14 23:29:06 +0000

    feat: add detector configuration for OpenVINO plugin

--- plugins/openvino/config.rs
+++ plugins/openvino/config.rs
@@ -6,10 +6,66 @@ use common::{
 };
 use serde::Deserialize;
 use serde_json::Value;
-use std::num::NonZeroU16;
+use std::{num::NonZeroU16, str::FromStr};
 use thiserror::Error;
+use toml;
 
-#[derive(Clone, Debug, PartialEq)]
+// Global configuration parsed from sentryshot.toml
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+pub(crate) struct Config {
+    pub(crate) host: String,
+    #[serde(rename = "model_name")]
+    pub(crate) model_name: String,
+    #[serde(rename = "input_tensor")]
+    pub(crate) input_tensor: String,
+    #[serde(rename = "output_tensor")]
+    pub(crate) output_tensor: String,
+}
+
+#[derive(Debug, Error)]
+pub(crate) enum ParseConfigError {
+    #[error("no table")]
+    NoTable,
+    #[error("no plugins")]
+    NoPlugins,
+    #[error("empty plugins")]
+    EmptyPlugins,
+    #[error("deserialize: {0}")]
+    Deserialize(#[from] toml::de::Error),
+    #[error("no openvino config found")]
+    NoOpenvinoConfig,
+}
+
+impl FromStr for Config {
+    type Err = ParseConfigError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use ParseConfigError::*;
+        let value: toml::Value = toml::from_str(s)?;
+        let toml::Value::Table(table) = value else {
+            return Err(NoTable);
+        };
+        let toml::Value::Array(plugins) = table.get("plugin").ok_or(NoPlugins)? else {
+            return Err(EmptyPlugins);
+        };
+        for plugin in plugins {
+            let toml::Value::Table(plugin) = plugin else {
+                continue;
+            };
+            let Some(toml::Value::String(name)) = plugin.get("name") else {
+                continue;
+            };
+            if name != "openvino" {
+                continue;
+            }
+            return Ok(plugin.to_owned().try_into::<Config>()?);
+        }
+        Err(NoOpenvinoConfig)
+    }
+}
+
+// Monitor-specific configuration parsed from the monitor's JSON config
+#[derive(Clone, Debug, PartialEq, Deserialize)]
 pub(crate) struct OpenvinoConfig {
     pub(crate) feed_rate: FeedRateSec,
     pub(crate) trigger_duration: DurationSec,
