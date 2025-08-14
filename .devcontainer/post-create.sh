c07bb9de46bb1f6bf2d5db9061f0c586c3fcdc97
Henry Ou <henryouly@gmail.com>
2025-08-14 18:01:53 +0000

    devcontainer: update postCreateCommand to use custom script for dependencies

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo apt update
+sudo apt install -y libavcodec-dev libavutil-dev libedgetpu-dev
