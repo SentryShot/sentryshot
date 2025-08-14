be2bec2d0c75517899fa5148c191f98998b78149
Henry Ou <henryouly@gmail.com>
2025-08-14 21:07:50 +0000

    fix: add libusb-1.0-0-dev to post-create.sh installation script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,4 +6,4 @@ echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sud
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo apt update
-sudo apt install -y libavcodec-dev libavutil-dev libedgetpu-dev
+sudo apt install -y libavcodec-dev libavutil-dev libedgetpu-dev libusb-1.0-0-dev
