8cdc378e3ed4107718c419a6c84cdf9f50ff479a
Henry Ou <henryouly@gmail.com>
2025-08-15 05:28:50 +0000

    feat: compile all protos

--- plugins/openvino/build.rs
+++ plugins/openvino/build.rs
@@ -1,7 +1,11 @@
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::configure().compile_protos(
-        &["proto/tensorflow_serving/apis/prediction_service.proto"],
-        &["proto/"],
-    )?;
+    let proto_files: Vec<PathBuf> = glob::glob("proto/**/*.proto")?
+        .filter_map(Result::ok)
+        .collect();
+
+    tonic_prost_build::configure()
+        .compile_protos(&proto_files, &[PathBuf::from("proto/")])?;
     Ok(())
 }
